f724766e997dcc07c9f28b44e5a63e6b59dfaebc
Vladimir Below <laitvladimir@yandex.ru>
2023-01-22 11:23:18 +0300

    N: Nsatroen redirect to the home page when you get to the page not found
    Signed-off-by: Vladimir Below <laitvladimir@yandex.ru>

--- src/components/pages/notFound.tsx
+++ src/components/pages/notFound.tsx
@@ -1,8 +1,23 @@
-import { createRoute } from "atomic-router";
+import { createRoute, redirect } from "atomic-router";
+import { createEvent } from "effector";
+import { onCleanup } from "solid-js";
+
+import { appRoute } from "./app";
 
 export const notFoundRoute = createRoute();
 
+const goHomeRedirect = createEvent();
+redirect({
+	clock: goHomeRedirect,
+	route: appRoute,
+});
+
+
 export const NotFound = () => {
+	const timeRedirect = setTimeout(() => { goHomeRedirect(); }, 2000);
+
+	onCleanup(() => clearTimeout(timeRedirect));
+
 	return (
 		<div
 			class="w-screen h-screen flex-center"
